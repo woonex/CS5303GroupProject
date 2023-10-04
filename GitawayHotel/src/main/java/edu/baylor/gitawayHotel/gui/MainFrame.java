e614f9bf6600b557b7c9fa0c00bccf8bd109ccb8
woonex <nwoolley9@gmail.com>
2023-10-04 00:02:06 -0500

    Refactor frame to not be an extension of JFrame but to instead contain an instance of JFrame

--- GitawayHotel/src/main/java/edu/baylor/gitawayHotel/gui/MainFrame.java
+++ GitawayHotel/src/main/java/edu/baylor/gitawayHotel/gui/MainFrame.java
@@ -1,15 +1,48 @@
 package edu.baylor.gitawayHotel.gui;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
-public class MainFrame extends JFrame {
+/**The frame that displays all of the gui components
+ * @author Nathan
+ *
+ */
+public class MainFrame {
+	private JFrame frame; 
+	private JPanel activePanel = new JPanel();
+	
+	/**Main constructor
+	 * 
+	 */
 	public MainFrame() {
-		super("Gitaway Hotel");
+		frame = new JFrame("Gitaway Hotel");
 		
-		setSize(400, 300);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 300);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		setVisible(true);
+		frame.setVisible(true);
+	}
+	
+	/**Adds the current panel to the gui and clears the last one and redraws the gui
+	 * @param panel
+	 */
+	public void add(JPanel panel) {
+		SwingUtilities.invokeLater(() -> {
+			frame.remove(activePanel);
+			frame.add(panel);
+			activePanel = panel;
+			frame.revalidate();
+			frame.repaint();
+		});
+		
+	}
+
+	/**Gets the frame object
+	 * @return the frame object
+	 */
+	public JFrame getFrame() {
+		return frame;
 	}
 }
