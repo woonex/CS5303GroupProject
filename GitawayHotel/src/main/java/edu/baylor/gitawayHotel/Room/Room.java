a2b465b18e86235b06c4283bbcb3cc7cdd7defb8
woonex <nwoolley9@gmail.com>
2023-10-03 21:30:52 -0500

    Add room boilerplate

---
+++ GitawayHotel/src/main/java/edu/baylor/gitawayHotel/Room/Room.java
@@ -0,0 +1,9 @@
+package edu.baylor.gitawayHotel.Room;
+
+/**A concept of a room that has attributes
+ * @author Nathan
+ *
+ */
+public class Room {
+	
+}
---
+++ GitawayHotel/src/main/java/edu/baylor/gitawayHotel/Room/RoomServices.java
@@ -0,0 +1,9 @@
+package edu.baylor.gitawayHotel.Room;
+
+/**Loading rooms from JSON from disk and managing saving
+ * @author Nathan
+ *
+ */
+public class RoomServices {
+
+}
