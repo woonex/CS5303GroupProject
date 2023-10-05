27dc4f48b06a7244ff10c266eaa5cb8d7b1a202a
Christen Barringer <barringerchristen4@gmail.com>
2023-10-04 21:23:57 -0700

    view rooms button set up

--- GitawayHotel/src/main/java/edu/baylor/gitawayHotel/gui/ClerkGui.java
+++ GitawayHotel/src/main/java/edu/baylor/gitawayHotel/gui/ClerkGui.java
@@ -1,27 +1,55 @@
 package edu.baylor.gitawayHotel.gui;
 
-import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import edu.baylor.gitawayHotel.user.UserType;
 
-/**The clerk is logged in and can either change their password or create or modify room information
+/**
+ * The clerk is logged in and can either change their password or create or
+ * modify room information
+ * 
  * @author Nathan
  *
  */
 public class ClerkGui extends AuthenticatedGui {
-	
+
+	private JButton viewRoomsButton;
+
 	public ClerkGui() {
 		super();
 	}
 
 	private JPanel panel;
-	
+
 	@Override
 	protected JPanel layoutMainArea() {
 		panel = new JPanel();
-		
-		panel.add(new JLabel("TODO WIP"));
+
+		// create an action panel to access common action buttons
+		JPanel actionPanel = new JPanel();
+		actionPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		viewRoomsButton = new JButton("View Rooms");
+		viewRoomsButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// Replace "NewPage" with the name of the new page you want to redirect to
+				ViewRoomsGui viewRoomsGui = new ViewRoomsGui();
+				//viewRoomsGui.setVisible(true);
+			}
+		});
+
+		actionPanel.add(viewRoomsButton);
+
+
+
+		panel.add(actionPanel, BorderLayout.NORTH);
+
 		return panel;
 	}
 
---
+++ GitawayHotel/src/main/java/edu/baylor/gitawayHotel/gui/ViewRoomsGui.java
@@ -0,0 +1,14 @@
+package edu.baylor.gitawayHotel.gui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import edu.baylor.gitawayHotel.user.UserType;
+
+public class ViewRoomsGui {
+    
+}
