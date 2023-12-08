d603a0d051a16272b543396e20b9045abd94a793
Nathan Woolley <nwoolley@albers.aero>
2023-12-07 22:01:22 -0600

    refactor display to reservation instead of billing gui

--- GitawayHotel/src/main/java/edu/baylor/gitawayHotel/gui/BillingGui.java
+++ GitawayHotel/src/main/java/edu/baylor/gitawayHotel/gui/BillingGui.java
@@ -16,8 +16,6 @@ public class BillingGui implements IGui {
 	private JPanel panel;
 	private JButton backButton;
 	private JEditorPane editorPane;
-	private static final String TAB = " ".repeat(6);
-	private static final String HALFTAB = " ".repeat(3);
 	
 	public BillingGui() {
 		doLayout();
@@ -46,65 +44,19 @@ public class BillingGui implements IGui {
 
 	public void setDisplay(User user, List<Reservation> reservations) {
 		StringBuilder sb = new StringBuilder();
-		double dayCost = 50;
+
 		sb.append("Guest: ");
 		sb.append(user.getUsername() + "\n\n");
 		
-		long totalCost = 0;
+		double totalCost = 0;
 		for (Reservation res : reservations) {
-			long qtyDays = ChronoUnit.DAYS.between(res.getStartDate(), res.getEndDate());
-			sb.append("Reservation:");
-			
-			sb.append("\n");
-			sb.append(TAB);
-			sb.append("Start date: ");
-			sb.append(res.getStartDate());
-			sb.append(TAB + "End Date: ");
-			sb.append(res.getEndDate());
-			sb.append("\n\n");
-			
-			sb.append(TAB);
-			sb.append("Days:");
-			sb.append(TAB.repeat(2));
-			sb.append("Day Cost");
-			sb.append(TAB.repeat(2));
-			sb.append("Modifier");
-			sb.append(TAB.repeat(2) + HALFTAB);
-			sb.append("Reservation Cost");
-			sb.append("\n");
-			
-			sb.append(TAB + HALFTAB);
-			sb.append(qtyDays);
-			sb.append(TAB + HALFTAB);
-			sb.append("*");
-			sb.append(TAB);
-			sb.append("$ " + String.format("%.2f", dayCost));
-			sb.append(TAB + HALFTAB);
-			sb.append("*");
-			sb.append(TAB + HALFTAB);
-			
-			double resCost;
-			if (res.wasCancelled()) {
-				sb.append("80%");
-				resCost = dayCost * qtyDays * .8;
-			} else {
-				sb.append("100%");
-				resCost = dayCost * qtyDays;
-			}
-			
-			sb.append(TAB);
-			sb.append("=");
-			sb.append(TAB.repeat(2) + "$" + String.format("%.2f", resCost));
-			if (res.wasCancelled()) {
-				sb.append("\n");
-				sb.append(TAB + "(Cancelled more than " + Reservation.RESERVATION_GRACE_DAYS + " days after reservation)");
-			}
-			sb.append("\n\n");			
+			sb.append(res.getFormattedCost());
+			double resCost = res.getFullCost();
 			totalCost += resCost;
 		}
 		
-		sb.append("TOTAL COST: $");
-		sb.append(totalCost);
+		sb.append("TOTAL BILL:" + " ".repeat(6) + "$ ");
+		sb.append(String.format("%.2f", totalCost));
 		setText(sb.toString());
 	}
 	
--- GitawayHotel/src/main/java/edu/baylor/gitawayHotel/reservation/Reservation.java
+++ GitawayHotel/src/main/java/edu/baylor/gitawayHotel/reservation/Reservation.java
@@ -1,6 +1,7 @@
 package edu.baylor.gitawayHotel.reservation;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 
 import edu.baylor.gitawayHotel.Room.Room;
@@ -149,6 +150,78 @@ public class Reservation implements Comparable<Reservation> {
 	
 	public void setCancelled() {
 		this.cancelled = true;
+	}
+	
+	public long getQtyDays() {
+		return ChronoUnit.DAYS.between(getStartDate(), getEndDate());
+	}
+	
+	public double getFullCost() {
+		double dayCost = getDayCost();
+		long qtyDays = getQtyDays();
+		
+		if (wasCancelled()) {
+			return dayCost * qtyDays * .8;
+		} else {
+			return dayCost * qtyDays;
+		}
+	}
+	
+	private static final String TAB = " ".repeat(6);
+	private static final String HALFTAB = " ".repeat(3);
+	
+	private double getDayCost() {
+		return 50;
+	}
+	
+	public String getFormattedCost() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Reservation:");
+		
+		sb.append("\n");
+		sb.append(TAB);
+		sb.append("Start date: ");
+		sb.append(getStartDate());
+		sb.append(TAB + "End Date: ");
+		sb.append(getEndDate());
+		sb.append("\n\n");
+		
+		sb.append(TAB);
+		sb.append("Days:");
+		sb.append(TAB.repeat(2));
+		sb.append("Day Cost");
+		sb.append(TAB.repeat(2));
+		sb.append("Modifier");
+		sb.append(TAB.repeat(2) + HALFTAB);
+		sb.append("Reservation Cost");
+		sb.append("\n");
+		
+		sb.append(TAB + HALFTAB);
+		sb.append(getQtyDays());
+		sb.append(TAB + HALFTAB);
+		sb.append("*");
+		sb.append(TAB);
+		sb.append("$ " + String.format("%.2f", getDayCost()));
+		sb.append(TAB + HALFTAB);
+		sb.append("*");
+		sb.append(TAB + HALFTAB);
+		
+		double resCost = getFullCost();
+		if (wasCancelled()) {
+			sb.append("80%");
+		} else {
+			sb.append("100%");
+		}
+		
+		sb.append(TAB);
+		sb.append("=");
+		sb.append(TAB.repeat(2) + "$ " + String.format("%.2f", resCost));
+		if (wasCancelled()) {
+			sb.append("\n");
+			sb.append(TAB + "(Cancelled more than " + Reservation.RESERVATION_GRACE_DAYS + " days after reservation)");
+		}
+		sb.append("\n\n");	
+		return sb.toString();
 	}
 
 	public boolean wasCancelled() {
